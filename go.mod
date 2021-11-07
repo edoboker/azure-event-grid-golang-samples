1a58a621739c8d995976a5b88701c84bf1da3080
Dodo <edoboker@gmail.com>
2021-11-07 23:57:45 +0200

    Initial hello world sample

---
+++ go.mod
@@ -0,0 +1,14 @@
+module example.com/charon
+
+go 1.16
+
+require (
+	github.com/Azure/azure-sdk-for-go v59.1.0+incompatible
+	github.com/Azure/azure-sdk-for-go/sdk/storage/azblob v0.2.0
+	github.com/Azure/go-autorest/autorest v0.11.22 // indirect
+	github.com/Azure/go-autorest/autorest/date v0.3.0 // indirect
+	github.com/Azure/go-autorest/autorest/validation v0.3.1 // indirect
+	github.com/Azure/go-autorest/tracing v0.6.0 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/google/uuid v1.3.0
+)
---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	eventgrid "github.com/Azure/azure-sdk-for-go/profiles/laeventGridClient/eventgrid/eventgrid"
+	"github.com/Azure/go-autorest/autorest"
+	"github.com/Azure/go-autorest/autorest/date"
+	uuid "github.com/google/uuid"
+)
+
+func main() {
+	eventGridKey := "<YOUR KEY HERE>"
+	eventGridTopicHostName := "<TOPIC HOSTNAME HERE IN THE FORMAT OF name.region.eventgrid.azure.net>"
+
+	keyAuthorizer := autorest.NewEventGridKeyAuthorizer(eventGridKey)
+	eventGridClient := eventgrid.BaseClient{}
+	eventGridClient.Authorizer = keyAuthorizer
+
+	id := uuid.New().String()
+	subject := "Sample-event"
+	eventType := "Demo"
+	dataVersion := "v2"
+
+	events := []eventgrid.Event{
+		{
+			ID:          &id,
+			Subject:     &subject,
+			EventType:   &eventType,
+			EventTime:   &date.Time{},
+			DataVersion: &dataVersion,
+			Data:        "Hello World!",
+		},
+	}
+
+	req, publishPrepareErr := eventGridClient.PublishEventsPreparer(context.Background(), eventGridTopicHostName, events)
+	if publishPrepareErr != nil {
+		fmt.Println(publishPrepareErr)
+	}
+
+	req, prepareErr := autorest.Prepare(req, eventGridClient.WithAuthorization())
+	if prepareErr != nil {
+		fmt.Println(prepareErr)
+	}
+
+	send, sendErr := eventGridClient.PublishEventsSender(req)
+	if sendErr != nil {
+		fmt.Println(sendErr)
+	}
+
+	resp, respondErr := eventGridClient.PublishEventsResponder(send)
+	if respondErr != nil {
+		fmt.Println(respondErr)
+	}
+
+	fmt.Println(resp.Status)
+}
